0880a581bd2e7c65add02ecb78e2b213e5ecf8fc
plla2 <jhbljs92@naver.com>
2023-09-06 19:02:50 +0900

    ✨ css 초기설정, themecolors설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/resetStyles.scss';
+import { ThemeProvider } from 'styled-components';
+import ThemeColors from './styles/ThemeColors.ts';
 import { Provider } from 'react-redux';
 import { store } from './redux/store/store.ts';
+import { StyledEngineProvider } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={ThemeColors}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ThemeProvider>
+    </StyledEngineProvider>
   </React.StrictMode>,
 );
---
+++ src/styles/ThemeColors.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from 'styled-components';
+
+const ThemeColors: DefaultTheme = {
+  colors: {
+    WHITE: '#fdfdfd',
+    BLACK: '#191919',
+    PURPLE_DARK: '#673ab8',
+    PURPLE_LIGHT: '#f0ebf8',
+    BLUE_DARK: '#4285f4',
+    BLUE_LIGHT: '#eff5fd',
+    GREY_DARK: '#6b6f74',
+    GREY_MEDIUM: '#f1f3f4',
+    GREY_LIGHT: '#f8f9fa',
+  },
+};
+
+export default ThemeColors;
