2c555e4c8476a255f3e26e90cbe09f6e75763b3d
plla2 <jhbljs92@naver.com>
2023-09-08 11:24:45 +0900

    🐛 preview page 인풋창 border 제거

--- src/components/QuestionOptions/TextQuestion/TextQuestionStyle.ts
+++ src/components/QuestionOptions/TextQuestion/TextQuestionStyle.ts
@@ -6,6 +6,7 @@ export const Wrapper = styled.div<{ type: string }>`
     line-height: 24px;
     width: ${({ type }) => (type === 'short' ? 50 : 82)}%;
     background: transparent;
+    border: none;
     border-bottom: 1px dashed ${({ theme }) => theme.color.grey_medium};
   }
 `;
